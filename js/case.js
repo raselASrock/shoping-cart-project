5190bc5d1fdd03b8912017eada214658d96a1535
raselASrock <mdraselm413@gmail.com>
2022-08-19 02:04:20 +0600

    phone-price-update-added

--- js/case.js
+++ js/case.js
@@ -28,19 +28,25 @@ function updateCaseNumber ( isIncreage ){
 }
 
 
+function updateCaseTotalPrice (newCaseNumber){
+    const caseTotalPrice = newCaseNumber * 59 ;
+   const caseTotalElement = document.getElementById('case-total');
+   caseTotalElement.innerText = caseTotalPrice;
+}
+
 
 
 document.getElementById('btn-case-plus').addEventListener('click', function(){
    const newCaseNumber =  updateCaseNumber( true )
    
-
-   const caseTotalPrice = newCaseNumber * 59 ;
-   const caseTotalElement = document.getElementById('case-total')
-   caseTotalElement.innerText = caseTotalPrice;
+    updateCaseTotalPrice(newCaseNumber)
+   
 })
 
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-    updateCaseNumber( false )
+    const newCaseNumber = updateCaseNumber( false )
+
+    updateCaseTotalPrice(newCaseNumber)
 
 })
