acfaa35ce3cc8f5b6480a0780b47555084d3e5af
raselASrock <mdraselm413@gmail.com>
2022-08-19 02:26:25 +0600

    phone-price-update

--- js/phone.js
+++ js/phone.js
@@ -1,10 +1,45 @@
 
 
 
-document.getElementById('btn-phone-plus').addEventListener('click', function(){
+
+function updatePhoneNumber(isIncreage) {
     const phoneNumberField = document.getElementById('phone-number-field');
-    const phoneNumberString = phoneNumberField.value ;
+    const phoneNumberString = phoneNumberField.value;
     const previousPhoneNumber = parseInt(phoneNumberString);
-    const newPhoneNumber = previousPhoneNumber + 1 ;
+    let newPhoneNumber;
+
+    if (isIncreage === true) {
+        newPhoneNumber = previousPhoneNumber + 1;
+    }
+    else {
+        newPhoneNumber = previousPhoneNumber - 1;
+    }
+
     phoneNumberField.value = newPhoneNumber;
+
+    return newPhoneNumber;
+}
+
+
+function updatePhoneTotalPrice( newPhoneNumber ) {
+    const phoneTotalPrice = newPhoneNumber * 1219;
+    const phoneTotalElement = document.getElementById('phone-total');
+    phoneTotalElement.innerText = phoneTotalPrice;
+}
+
+
+
+document.getElementById('btn-phone-plus').addEventListener('click', function () {
+
+    const newPhoneNumber = updatePhoneNumber(true);
+
+    updatePhoneTotalPrice(newPhoneNumber)
+})
+
+
+document.getElementById('btn-phone-minus').addEventListener('click', function () {
+
+    const newPhoneNumber = updatePhoneNumber(false)
+
+    updatePhoneTotalPrice(newPhoneNumber)
 })
